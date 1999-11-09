6a73c93c05921800fc3bdf64acacb503c057073a
Gerd Lanfermann <lanfer@aei.mpg.de>
1999-11-09 09:17:18 +0000

    IDBrillData: adding startup file which setups symmetries for brillconf
    git-svn-id: https://svn.einsteintoolkit.org/cactus/EinsteinInitialData/IDBrillData/trunk@10 a678b1cf-93e1-4b43-a69d-d43939e66649

---
+++ IDBrillData/src/Startup.c
@@ -0,0 +1,17 @@
+#include "cctk.h"
+#include "cctk_arguments.h"
+
+
+void BrilData_InitSymBound(CCTK_CARGUMENTS)
+{ 
+  DECLARE_CCTK_CARGUMENTS
+  int sym[3];
+
+  sym[0] = 1;
+  sym[1] = 1;
+  sym[2] = 1;
+  
+
+
+  SetCartSymmetry(cctkGH, sym,"IDBrillData::brillpsi");
+}
