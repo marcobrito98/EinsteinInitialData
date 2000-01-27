1ca07c5794d077fd2415476939c45440f7796d2c
Gabrielle Allen <gdallen@illinois.edu>
2000-01-27 07:46:08 +0000

    IDAnalyticBH: cctk_parameters -> cctk_Parameters cctk_arguments -> cctk_Arguments
    git-svn-id: https://svn.einsteintoolkit.org/cactus/EinsteinInitialData/IDAnalyticBH/trunk@59 6a3ddf76-46e1-4315-99d9-bc56cac1ef84

--- IDAnalyticBH/src/ParamChecker.c
+++ IDAnalyticBH/src/ParamChecker.c
@@ -12,8 +12,8 @@
 #include <stdlib.h>
 
 #include "cctk.h"
-#include "cctk_arguments.h"
-#include "cctk_parameters.h"
+#include "cctk_Arguments.h"
+#include "cctk_Parameters.h"
 
 #include "CactusEinstein/Einstein/src/Einstein.h"
 
