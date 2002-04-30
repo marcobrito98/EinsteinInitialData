df7d8dc44b91cfecbe45e1511cf981da5241b31d
Gabrielle Allen <gdallen@illinois.edu>
2002-04-30 08:53:36 +0000

    Exact: Finished converting Exact, which is now independent of StaticConformal, and thus only runs with metric_type physical
    git-svn-id: https://svn.einsteintoolkit.org/cactus/EinsteinInitialData/Exact/trunk@54 e296648e-0e4f-0410-bd07-d597d9acff87

---
+++ Exact/src/ParamCheck.c
@@ -0,0 +1,73 @@
+ /*@@
+   @file      ParamCheck.c
+   @date      Ues April 30 2002
+   @author    Gabrielle Allen
+   @desc 
+   Parameter checking stuff for Exact
+   @enddesc
+   @version $Header$
+ @@*/
+
+#include "cctk.h"
+
+#include "cctk_Arguments.h"
+#include "cctk_Parameters.h"
+
+static const char *rcsid = "$Header$";
+
+CCTK_FILEVERSION(AEIDevelopment_Exact_ParamCheck_c)
+
+/********************************************************************
+ *********************     Local Data Types   ***********************
+ ********************************************************************/
+
+/********************************************************************
+ ********************* Local Routine Prototypes *********************
+ ********************************************************************/
+
+/********************************************************************
+ ***************** Scheduled Routine Prototypes *********************
+ ********************************************************************/
+
+void Exact_ParamCheck(CCTK_ARGUMENTS);
+
+/********************************************************************
+ ********************* Other Routine Prototypes *********************
+ ********************************************************************/
+
+/********************************************************************
+ *********************     Local Data   *****************************
+ ********************************************************************/
+
+/********************************************************************
+ *********************     External Routines   **********************
+ ********************************************************************/
+
+ /*@@
+   @routine    Exact_ParamCheck
+   @date       Tue April 30 2002
+   @author     Tom Goodale
+   @desc 
+   Scheduled routine to detect invalid parameter settings.
+   @enddesc 
+   @calls     
+   @calledby   
+   @history 
+ 
+   @endhistory 
+ 
+ @@*/
+void Exact_ParamCheck(CCTK_ARGUMENTS)
+{
+  DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_PARAMETERS;
+
+  if(! CCTK_EQUALS(metric_type, "physical"))
+  {
+    CCTK_PARAMWARN("Unknown ADMBase::metric_type - known types are \"physical\"");
+  }
+}
+
+/********************************************************************
+ *********************     Local Routines   *************************
+ ********************************************************************/
