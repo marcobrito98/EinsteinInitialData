9ce93eddfcf2ce462a5d9d02fb14b07d8832b474
Gabrielle Allen <gdallen@illinois.edu>
2002-05-17 15:36:33 +0000

    IDBrillData: Tidying so that the conformal solver isn't used to solve for the conformal factor, in fact the conformal factor is now only set at the end of the thorn.
    Now if the conformal metric is used, psi will be set to the conformal factor
    which was actually solved for. This means that right now that you can't use IDBrillData with conformal derivatives, although it would presumably be
    easy enough to numerically calculate them, or to return 1 and 0's instead
    just for this case.
    
    git-svn-id: https://svn.einsteintoolkit.org/cactus/EinsteinInitialData/IDBrillData/trunk@76 a678b1cf-93e1-4b43-a69d-d43939e66649

--- IDBrillData/src/ParamCheck.c
+++ IDBrillData/src/ParamCheck.c
@@ -49,18 +49,26 @@ void IDBrillData_ParamChecker(CCTK_ARGUMENTS)
   DECLARE_CCTK_PARAMETERS
 
   /* Do we know how to deal with this type of metric ? */
-  if( ! CCTK_EQUALS(metric_type, "physical") && 
-      ! CCTK_EQUALS(metric_type, "static conformal"))
+  if( ! CCTK_Equals(metric_type, "physical") && 
+      ! CCTK_Equals(metric_type, "static conformal"))
   {
     CCTK_PARAMWARN("Unknown ADMBase::metric_type - known types are \"physical\" and \"static conformal\"");
   }
 
+  if (CCTK_Equals(metric_type, "static conformal"))
+  {
+    if (!CCTK_Equals(conformal_storage,"factor"))
+    {
+      CCTK_PARAMWARN("BrillData only sets the conformal factor (this could easily be changed ... please ask)");
+    }
+  }
+
   CCTK_INFO("Setting up Brill data");
-  if (CCTK_EQUALS(metric_type, "static conformal"))
+  if (CCTK_Equals(metric_type, "static conformal"))
   {
      CCTK_VInfo(CCTK_THORNSTRING,"  ... using trivial conformal %s",conformal_storage);
   }
-  else if (CCTK_EQUALS(metric_type, "physical"))
+  else if (CCTK_Equals(metric_type, "physical"))
   {
     CCTK_INFO("  ... using physical metric");
   }
