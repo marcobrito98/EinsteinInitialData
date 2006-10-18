ef5c3761e868e1185ccbb61ba3a9e7973baa7b89
Denis Pollney <denis.pollney@gmail.com>
2006-10-18 12:04:19 +0000

    TwoPunctures: Added center_offset parameter to allow the initial data BHs to be shifted around the grid.
    git-svn-id: https://svn.einsteintoolkit.org/cactus/EinsteinInitialData/TwoPunctures/trunk@70 b2a53a04-0f4f-0410-87ed-f9f25ced00cf

--- TwoPunctures/src/TwoPunctures.c
+++ TwoPunctures/src/TwoPunctures.c
@@ -307,6 +307,16 @@ TwoPunctures (CCTK_ARGUMENTS)
   }
 
   for (k = imin[2]; k < imax[2]; ++k)
+    for (j = imin[1]; j < imax[1]; ++j)
+      for (i = imin[0]; i < imax[0]; ++i)
+        {
+          int ijk = CCTK_GFINDEX3D(cctkGH, i, j, k);
+          x[ijk] -= center_offset[0];
+          y[ijk] -= center_offset[1];
+          z[ijk] -= center_offset[2];
+        }
+
+  for (k = imin[2]; k < imax[2]; ++k)
   {
     for (j = imin[1]; j < imax[1]; ++j)
     {
@@ -542,6 +552,16 @@ TwoPunctures (CCTK_ARGUMENTS)
       } /* for i */
     }   /* for j */
   }     /* for k */
+
+  for (k = imin[2]; k < imax[2]; ++k)
+    for (j = imin[1]; j < imax[1]; ++j)
+      for (i = imin[0]; i < imax[0]; ++i)
+        {
+          int ijk = CCTK_GFINDEX3D(cctkGH, i, j, k);
+          x[ijk] += center_offset[0];
+          y[ijk] += center_offset[1];
+          z[ijk] += center_offset[2];
+        }
 
   if (use_sources && rescale_sources)
   {
