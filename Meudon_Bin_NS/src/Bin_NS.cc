64fc4568756a6605fe6a54d414525b6b810434dc
Roland Haas <rhaas@tapir.caltech.edu>
2012-01-26 17:03:48 +0000

    Meudon_Bin_NS: catch ios::failure exception from Bin_NS and transfer to CCTK_Warn
    git-svn-id: https://svn.einsteintoolkit.org/cactus/EinsteinInitialData/Meudon_Bin_NS/trunk@15 f2ea251b-07d6-4a45-8f2c-a162b3fa7596

--- Meudon_Bin_NS/src/Bin_NS.cc
+++ Meudon_Bin_NS/src/Bin_NS.cc
@@ -4,6 +4,7 @@
 #include <cstdio>
 #include <cassert>
 #include <vector>
+#include <ios>
 
 #include <cctk.h>
 #include <cctk_Arguments.h>
@@ -80,6 +81,7 @@ void Meudon_Bin_NS_initialise (CCTK_ARGUMENTS)
 
   CCTK_VInfo (CCTK_THORNSTRING, "Reading from file \"%s\"", filename);
 
+  try {
   Bin_NS bin_ns (npoints, &xx[0], &yy[0], &zz[0], filename);
 
   CCTK_VInfo (CCTK_THORNSTRING, "omega [rad/s]:       %g", bin_ns.omega);
@@ -196,5 +198,9 @@ void Meudon_Bin_NS_initialise (CCTK_ARGUMENTS)
   }
 
   CCTK_INFO ("Done.");
+  } catch (ios::failure e) {
+    CCTK_VWarn (CCTK_WARN_ABORT, __LINE__, __FILE__, CCTK_THORNSTRING,
+                "Could not read initial data from file '%s': %s", filename, e.what());
+  }
 }
 
