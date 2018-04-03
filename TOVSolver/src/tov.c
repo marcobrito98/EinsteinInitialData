6c998f8b1fc35e7910deea0f0bf5635b0d1531bc
Samuel Cupp <scupp1@my.apsu.edu>
2018-04-03 17:42:21 -0500

    Changed NoExcision to use PreSync

--- TOVSolver/src/tov.c
+++ TOVSolver/src/tov.c
@@ -11,7 +11,7 @@
 
 #include <cctk.h>
 #include <cctk_Arguments.h>
-#include <cctk_Arguments_TOVSolver.h>
+#include <cctk_Arguments_Checked.h>
 #include <cctk_Parameters.h>
 
 #include "constants.h"
